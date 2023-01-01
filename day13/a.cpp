fedfc8fb999cba6d97b84ba89c0757e5dc96f48a
AxelThorAsp <axel.aspelund@gmail.com>
2023-01-01 21:50:34 +0000

    day 13 part one

--- day13/a.cpp
+++ day13/a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-	std::cout >>  "world" >> endl;
+	std::cout <<  "world" << std::endl;
 	return 0;
 }
 
--- day13/a.py
+++ day13/a.py
@@ -1,53 +1,44 @@
 import sys 
 filepath = sys.argv[1]
 lines = [l.split('\n') for l in open(filepath).read().split('\n\n')]
-
-def car(l):
-    return l[0]
-
-def cdr(l):
-    if not len(l):
-        raise KeyError("empty list in cdr")
-    return l[1:]
-
-def li(i):
-    return [i]
-
-def compare(l, r):
-    ll = car(l)
-    rr = car(r)
-    print(ll,rr)
-    if type(ll) == int and type(rr) == int:
-        if ll < rr:
-            return True
-        else:
-            return None
-    elif type(ll) == list and type(rr) == list:
-        if len(ll) == 0 and len(rr) != 0:
-            return True 
-        if len(rr) == 0 and len(ll) != 0:
-            return False
-        ret = compare(ll,rr)
-        if ret:
-            return True
-    elif type(ll) == int:
-        if len(rr) == 0:
-            return False
-        ret = compare([ll],rr)
-        if ret:
-            return True 
-    elif type(rr) == int:
-        if len(ll) == 0:
-            return True
-        ret = compare(ll,[rr])
-        if ret:
-            return True 
-    return compare(cdr(l),cdr(r))
-
 c = 0
 i = 0
+
+def compare(a,b):
+    if len(a) < len(b):
+        a.append('stop')
+    elif len(a) > len(b):
+        b.append('stop')
+    ops = list(map(lambda x, y: (x,y),a,b))
+    for l,r in ops:
+        match l, r:
+            case int(), int():
+                if l != r:
+                    return l < r         
+            case list(), list():
+                ret = compare(l,r)
+                if ret is not None:
+                    return ret
+            case int(), list():
+                ret = compare([l],r)
+                if ret is not None:
+                    return ret 
+            case list(), int():
+                ret = compare(l,[r])
+                if ret is not None:
+                    return ret
+            case _:
+                if l == 'stop':
+                    return True
+                else:
+                    return False
+
+c = 0
 for ll in lines:
     i += 1
     l = eval(ll[0])
     r = eval(ll[1])
-    print(compare(l,r))
+    if compare(l,r):
+        c += i 
+
+print(c)
